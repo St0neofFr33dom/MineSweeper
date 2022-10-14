d1db271829030f864996cf37b566aadbf9250fc8
St0neofFr33dom <christophefreeston@googlemail.com>
2022-10-14 16:26:53 +0100

    On a safe clicked space, the number inside is is different depending on the number

--- components/Tile/index.js
+++ components/Tile/index.js
@@ -7,18 +7,17 @@ const Tile = ({mine}) => {
 
     function reveal(e){
         if(e.target.dataset.status !== 'unclicked'){return}
-        switch(e.target.dataset.content){
-            case 'M':
+        if(e.target.dataset.content === 'M'){
                 e.target.className = styles.mine
                 e.target.dataset.status = 'clicked'
                 setTileContent(e.target.dataset.content)
                 return
-            default:
-                e.target.className = styles.tile
-                e.target.dataset.status = 'clicked'
-                setTileContent(e.target.dataset.content)
-                return
         }
+        e.target.className = styles.tile
+        e.target.dataset.status = 'clicked'
+        setTileContent(e.target.dataset.content)
+        return
+        
     }
 
 
@@ -43,9 +42,24 @@ const Tile = ({mine}) => {
         }
     }
 
+    function getColour(content){
+        switch(content){
+            case 0: return {color:"aqua"}
+            case 1: return {color:"#000"}
+            case 2: return {color:"#909"}
+            case 3: return {color:"#009"}
+            case 4: return {color:"#090"}
+            case 5: return {color:"#990"}
+            case 6: return {color:"#940"}
+            case 7: return {color:"#900"}
+            case 8: return {color:"#904"}
+            case 'M': return {color:"#000"}
+            default: console.log('Error')
+        }
+    }
 
     return (
-        <div className={styles.unclicked} onClick={(e)=>{reveal(e)}} onContextMenu={(e)=>{mark(e)}} data-content={mine} data-status='unclicked'>
+        <div className={styles.unclicked} style={getColour(mine)} onClick={(e)=>{reveal(e)}} onContextMenu={(e)=>{mark(e)}} data-content={mine} data-status='unclicked'>
             {tileContent}
         </div>
     )
