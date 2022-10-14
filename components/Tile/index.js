381751a4ebf4c0b417123df23df22a3fc170320d
St0neofFr33dom <christophefreeston@googlemail.com>
2022-10-14 15:41:19 +0100

    Can add flags and question marks to unclikced squares

--- components/Tile/index.js
+++ components/Tile/index.js
@@ -6,21 +6,46 @@ const Tile = ({mine}) => {
     const [tileContent,setTileContent] = useState()
 
     function reveal(e){
-        if(e.target.className !== styles.unclicked){return}
+        if(e.target.dataset.status !== 'unclicked'){return}
         switch(e.target.dataset.content){
             case 'M':
                 e.target.className = styles.mine
+                e.target.dataset.status = 'clicked'
                 setTileContent(e.target.dataset.content)
                 return
             default:
                 e.target.className = styles.tile
+                e.target.dataset.status = 'clicked'
                 setTileContent(e.target.dataset.content)
                 return
         }
     }
 
+
+    function mark(e){
+        e.preventDefault()
+        if(e.target.dataset.status === 'clicked'){return}
+        switch(e.target.dataset.status){
+            case 'unclicked':
+                e.target.dataset.status = 'marked'
+                setTileContent("F")
+                return
+            case 'marked':
+                e.target.dataset.status = 'unsure'
+                setTileContent("?")
+                return
+            case 'unsure':
+                e.target.dataset.status = 'unclicked'
+                setTileContent("")
+                return
+            default:
+                return
+        }
+    }
+
+
     return (
-        <div className={styles.unclicked} onClick={(e)=>{reveal(e)}} data-content={mine}>
+        <div className={styles.unclicked} onClick={(e)=>{reveal(e)}} onContextMenu={(e)=>{mark(e)}} data-content={mine} data-status='unclicked'>
             {tileContent}
         </div>
     )
