a34ec1b577f0e38bb44f5821b20188f41ccc5172
St0neofFr33dom <christophefreeston@googlemail.com>
2022-10-15 16:28:36 +0100

    moved the reducer function to it's own file to rreduce cognitive overload

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,12 @@
 import styles from "../styles/Home.module.css";
-import { useState, useReducer, useContext } from "react";
+import { useState, useReducer} from "react";
 import Board from "../components/Board";
-import getAdjacent from "../funcs/getAdjacent";
+import reducer from "../funcs/reducer";
 import gameContext from "../context/gameContext";
 
 export default function Home() {
 
-  function reducer(state, action) {
-    switch (action.type) {
-      case "newGame": {
-        return { ...state, settings: true };
-      }
-      case "difficulty": {
-        switch(action.value){
-          case 'easy': return {...state, difficulty:action.value, width:7,height:7, mines:10}
-          case 'medium': return {...state, difficulty:action.value, width:12,height:12, mines:40}
-          case 'hard': return {...state, difficulty:action.value, width:20,height:20, mines:80}
-          case 'custom': return {...state, difficulty:action.value}
-          default: return state
-        }
-      }
-      case "changeNumber":
-        return { ...state, [action.field]: action.value };
-      case "createGame":
-        let minePositions = [];
-        let gridSize = state.width * state.height;
-        while (minePositions.length < state.mines) {
-          let position = Math.floor(Math.random() * gridSize);
-          if (!minePositions.includes(position)) {
-            minePositions.push(position);
-          }
-        }
-        let gameGrid = [];
-        for (let i = 0; i < state.height; i++) {
-          let row = [];
-          for (let j = 0; j < state.width; j++) {
-            row.push(false);
-          }
-          gameGrid.push(row);
-        }
-        for (let i = 0; i < minePositions.length; i++) {
-          let position = minePositions[i];
-          let rowPosition = Math.floor(position / state.width);
-          let columnPosition = position % state.width;
-          gameGrid[rowPosition][columnPosition] = "M";
-        }
-        let game = getAdjacent(gameGrid);
-        return { ...state, game: game, settings: false, flagsLeft: state.mines };
-      case 'decrementFlag':
-        return {...state, flagsLeft: state.flagsLeft - 1}
-      case 'incrementFlag':
-        return {...state, flagsLeft: state.flagsLeft + 1}
-      default:
-        return state;
-    }
-  }
+  
 
   const [state, dispatch] = useReducer(reducer, {
     settings: true,
@@ -138,13 +90,6 @@ export default function Home() {
           }}
         >
           New Game
-        </button>
-        <button
-          onClick={() => {
-            dispatch({ type: "createGame" });
-          }}
-        >
-          Reset
         </button>
       </main>
       </gameContext.Provider>
