d64c1af7047f54e4e5af5817b0ba4eb5035516b4
St0neofFr33dom <christophefreeston@googlemail.com>
2022-10-15 13:28:44 +0100

    fixed glitch where flag text colour changes colour to match the colour of the text underneath it

--- components/Tile/index.js
+++ components/Tile/index.js
@@ -5,6 +5,8 @@ const Tile = ({mine}) => {
 
     const [tileContent,setTileContent] = useState()
 
+    const [tileStyle,setTileStyle] = useState({color:'black'})
+
     function reveal(e){
         if(e.target.dataset.status !== 'unclicked'){return}
         if(e.target.dataset.content === 'M'){
@@ -14,6 +16,8 @@ const Tile = ({mine}) => {
                 return
         }
         e.target.className = styles.tile
+        let textColour = getColour(e.target.dataset.content)
+        setTileStyle(textColour)
         e.target.dataset.status = 'clicked'
         setTileContent(e.target.dataset.content)
         return
@@ -44,22 +48,22 @@ const Tile = ({mine}) => {
 
     function getColour(content){
         switch(content){
-            case 0: return {color:"aqua"}
-            case 1: return {color:"#000"}
-            case 2: return {color:"#909"}
-            case 3: return {color:"#009"}
-            case 4: return {color:"#090"}
-            case 5: return {color:"#990"}
-            case 6: return {color:"#940"}
-            case 7: return {color:"#900"}
-            case 8: return {color:"#904"}
+            case '0': return {color:"aqua"}
+            case '1': return {color:"#000"}
+            case '2': return {color:"#909"}
+            case '3': return {color:"#009"}
+            case '4': return {color:"#090"}
+            case '5': return {color:"#990"}
+            case '6': return {color:"#940"}
+            case '7': return {color:"#900"}
+            case '8': return {color:"#904"}
             case 'M': return {color:"#000"}
             default: console.log('Error')
         }
     }
 
     return (
-        <div className={styles.unclicked} style={getColour(mine)} onClick={(e)=>{reveal(e)}} onContextMenu={(e)=>{mark(e)}} data-content={mine} data-status='unclicked'>
+        <div className={styles.unclicked} style={tileStyle} onClick={(e)=>{reveal(e)}} onContextMenu={(e)=>{mark(e)}} data-content={mine} data-status='unclicked'>
             {tileContent}
         </div>
     )
