c291db1aa055416bdb508b75557784be8a1193d7
St0neofFr33dom <christophefreeston@googlemail.com>
2022-10-21 19:28:57 +0100

    removed excess code from clickAdjacent function

--- funcs/clickAdjacent.js
+++ funcs/clickAdjacent.js
@@ -2,38 +2,30 @@ export default function clickAdjacent(gameGrid,click,row,column){
     if (gameGrid[row - 1] !== undefined) {
         if(gameGrid[row - 1][column - 1] !== undefined && gameGrid[row - 1][column - 1]["status"] == "unclicked"){
             click(row-1,column-1)
-            array.push([row-1,column-1])
         }
         if(gameGrid[row - 1][column ] !== undefined && gameGrid[row - 1][column ]["status"] == "unclicked"){
             click(row-1,column)
-            array.push([row-1,column])
         }
         if(gameGrid[row - 1][column + 1] !== undefined && gameGrid[row - 1][column + 1]["status"] == "unclicked"){
             click(row-1,column+1)
-            array.push([row-1,column+1])
         }
     }
     if (gameGrid[row+1] !== undefined){
         if(gameGrid[row + 1][column - 1] !== undefined && gameGrid[row + 1][column - 1]["status"] == "unclicked"){
             click(row+1,column-1)
-            array.push([row+1,column-1])
         }
         if(gameGrid[row + 1][column ] !== undefined && gameGrid[row + 1][column]["status"] == "unclicked"){
             click(row+1,column)
-            array.push([row+1,column])
         }
         if(gameGrid[row + 1][column + 1] !== undefined  && gameGrid[row + 1][column + 1]["status"] == "unclicked"){
             click(row+1,column+1)
-            array.push([row+1,column+1])
         }
     }
     if(gameGrid[row][column-1] !== undefined  && gameGrid[row][column - 1]["status"] == "unclicked"){
         click(row,column-1)
-        array.push([row,column-1])
     }
     if(gameGrid[row][column+1] !== undefined  && gameGrid[row][column + 1]["status"] == "unclicked"){
         click(row,column+1)
-        array.push([row,column+1])
     }
-    return array
+    return
 }
